dab04eb21a9e6a617a9bee1c2b4aaa2186265ab0
Kashifalii <alyyk82@gmail.com>
2024-06-04 21:01:02 +0500

    2nd commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,4 @@
 // Initialize Swiper
-
 const progressCircle = document.querySelector(".autoplay-progress svg");
 const progressContent = document.querySelector(".autoplay-progress span");
 var swiper = new Swiper(".mySwiper", {
@@ -22,5 +21,21 @@ var swiper = new Swiper(".mySwiper", {
             progressCircle.style.setProperty("--progress", 1 - progress);
             progressContent.textContent = `${Math.ceil(time / 1000)}s`;
         }
+    }
+});
+
+// navbar
+
+let navBtn = document.querySelector('.offcanvas-btn');
+let navContent = document.querySelector('.nav-content');
+
+navBtn.addEventListener('click', () => {
+    navBtn.classList.toggle('active');
+    if (navBtn.classList.contains('active')) {
+        navContent.style.left = 0;
+        navContent.style.transition = "all ease .5s";
+    } else {
+        navContent.style.left = "100%";
+
     }
 });
